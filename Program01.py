c8ce2c8c2aaa470471e858ac72483ec6bbb0a555
HaroldAmad <haroldpamad@iskolarngbayan.pup.edu.ph>
2021-11-01 20:53:36 +0800

    Final Commit with casing errors corrections and accuracy

--- Program01.py
+++ Program01.py
@@ -1,16 +1,16 @@
 def getApple ():
-    apple_func = int(input("Apple: "))
+    apple_func = int(input("Apples you want to buy: "))
     return apple_func
 
 def getOrange ():
-    orange_func = int(input("Orange: "))
+    orange_func = int(input("Oranges you want to buy: "))
     return orange_func
 
 def gettotalPrice ():
-     totalPrice_func = ((apple*20) + (orange*25))
-     return totalPrice_func
+     totalprice_func = ((apple*20) + (orange*25))
+     return totalprice_func
 
-def displayOutput (totalPriceF,):
+def displayOutput (totalPriceF):
     print(f"The total amount is {totalPriceF}")
 
 apple = getApple()
