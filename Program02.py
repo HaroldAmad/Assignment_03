9ec4046ef1d52fc4445ea4904b41ca6c45b32740
HaroldAmad <haroldpamad@iskolarngbayan.pup.edu.ph>
2021-11-01 20:40:13 +0800

    Total Apples and Change Function

--- Program02.py
+++ Program02.py
@@ -3,5 +3,18 @@ def getMoney():
     return money_Func
 
 def getapplePrice():
-    appleprice_Func = int(input("Price of Apple"))
+    appleprice_Func = int(input("Price of Apple: "))
     return appleprice_Func
+
+def gettotalApples():
+    totalapples_Func = (money/applePrice)
+    return totalapples_Func
+
+def getChange():
+    change_Func = (money%applePrice)
+    return change_Func   
+
+money = getMoney()
+applePrice = getapplePrice()
+totalApples = gettotalApples()
+change = getChange()    
