99efa548d6b353ede17cbff7f157ea421adcb1bf
HaroldAmad <haroldpamad@iskolarngbayan.pup.edu.ph>
2021-11-01 20:04:41 +0800

    Total Price Function

--- Program01.py
+++ Program01.py
@@ -3,7 +3,13 @@ def getApple ():
     return apple_func
 
 def getOrange ():
-    orange_func = input("Orange:")
+    orange_func = input("Orange: ")
     return orange_func
 
-    
+def gettotalPrice ():
+     totalPrice_func = int((apple * 20) + (orange * 25))
+     return totalPrice_func
+
+apple = getApple()
+orange = getOrange()
+totalPrice = gettotalPrice()
