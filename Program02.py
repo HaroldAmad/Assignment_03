425f3ef3f518732a85af121d61f015c870ac50d2
HaroldAmad <haroldpamad@iskolarngbayan.pup.edu.ph>
2021-11-01 20:31:17 +0800

    Amount of Apple and Price Function

---
+++ Program02.py
@@ -0,0 +1,7 @@
+def getMoney():
+    money_Func = int(input("Amoount of Money: "))
+    return money_Func
+
+def getapplePrice():
+    appleprice_Func = int(input("Price of Apple"))
+    return appleprice_Func
