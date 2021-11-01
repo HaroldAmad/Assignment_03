0e3b8c1f5c9968bcf17f56776a2187d5421c098b
HaroldAmad <haroldpamad@iskolarngbayan.pup.edu.ph>
2021-11-01 20:15:42 +0800

    Final Commit

--- Program01.py
+++ Program01.py
@@ -1,15 +1,19 @@
 def getApple ():
-    apple_func = input("Apple: ")
+    apple_func = int(input("Apple: "))
     return apple_func
 
 def getOrange ():
-    orange_func = input("Orange: ")
+    orange_func = int(input("Orange: "))
     return orange_func
 
 def gettotalPrice ():
-     totalPrice_func = int((apple * 20) + (orange * 25))
+     totalPrice_func = ((apple*20) + (orange*25))
      return totalPrice_func
+
+def displayOutput (totalPriceF,):
+    print(f"The total amount is {totalPriceF}")
 
 apple = getApple()
 orange = getOrange()
 totalPrice = gettotalPrice()
+displayOutput (totalPrice)
